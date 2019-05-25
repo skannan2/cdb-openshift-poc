bb6d3af63f52bc9329fee3aad608f14ddeec850a
370567 <370567@192.168.1.8>
2019-05-25 21:13:42 +0530

    updated

--- usermgmtservice/src/main/java/com/cognizant/openshift/poc/usermgmtservice/security/SecurityConstants.java
+++ usermgmtservice/src/main/java/com/cognizant/openshift/poc/usermgmtservice/security/SecurityConstants.java
@@ -6,6 +6,6 @@ public class SecurityConstants {
     public static final String EXPIRATION_TIME = "86400000";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/user/create";
+    public static final String SIGN_UP_URL = "/create";
     public static final String LOGIN_URL = "/login";
 }
--- usermgmtservice/src/main/java/com/cognizant/openshift/poc/usermgmtservice/security/WebSecurity.java
+++ usermgmtservice/src/main/java/com/cognizant/openshift/poc/usermgmtservice/security/WebSecurity.java
@@ -23,7 +23,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
         		.authorizeRequests()
-                //.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
                 //.antMatchers(HttpMethod.POST, SecurityConstants.LOGIN_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
